afce6e06b3def26aa3efee41f6bf824b4a61de72
Mina Abouei <marva.abouei@gmail.com>
2020-03-20 14:34:18 +0100

    bartender design and comments

--- SailingInSunsetProject/js/index.js
+++ SailingInSunsetProject/js/index.js
@@ -1,7 +1,8 @@
-function gotoCustomerPage() {
+
+function gotoCustomerPage() {  /* function for transition to customer page */
   window.location.href = "customer.html";
 }
 
-function gotoBartenderPage() {
+function gotoBartenderPage() { /* function for transition to bartender page */
   window.location.href = "bartender-home.html";
 }
