eb15b62993e9986bda97ffbc3280ce865d98b127
unknown <sabuj.cse.diu@gmail.com>
2020-03-28 20:57:50 +0100

    updates

--- SailingInSunsetProject/js/index.js
+++
@@ -1,8 +0,0 @@
-
-function gotoCustomerPage() {  /* function for transition to customer page */
-  window.location.href = "customer.html";
-}
-
-function gotoBartenderPage() { /* function for transition to bartender page */
-  window.location.href = "bartender-home.html";
-}
